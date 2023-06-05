089efad6b3eb75b1d6cf118635c9771ff00edf92
Ralph Giles <rgiles@brave.com>
2023-06-05 12:46:05 -0700

    Update pi-rs-cli-utils to clap 4
    Address unsound and unmaintaned `cargo audit` warnings about
    transitive dependencies by updating the latest release of the
    command-line argument parsing crate. This involves porting to
    the new api. Switch to the `derive` interface to shorted the
    implementation code.
    
    Remove unused direct dependency on `clap` in the top-level crate.

--- pi-rs-cli-utils/src/lib.rs
+++ pi-rs-cli-utils/src/lib.rs
@@ -1,87 +1,43 @@
-use clap::{App, Arg};
+use clap::{Parser, arg};
 use std::env;
+use std::num::ParseIntError;
 
+#[derive(Parser)]
+/// Flags for setting PIR parameters
 pub struct CLIFlags {
-  pub m: usize,
+  /// Log2 of height of DB matrix
+  #[arg(short, long, default_value_t = 16, value_parser = parse_exp_to_usize)]
+  pub matrix_height: usize,
+  /// LWE dimension
+  #[arg(short = 'd', long = "dim", default_value_t = 2048)]
   pub lwe_dim: usize,
+  /// Number of plaintext bits encoded in each entry of DB matrix
+  #[arg(short, long, default_value_t = 10)]
   pub plaintext_bits: usize,
+  /// Log2 of element bit length
+  #[arg(short, long, default_value_t = 13, value_parser = parse_exp_to_usize)]
   pub ele_size: usize,
 }
 
 pub fn parse_cli_flags() -> CLIFlags {
-  let matches = App::new("PIR example")
-    .version("0.0.1")
-    .author("Alex Davidson <coela@alxdavids.xyz>")
-    .about("Flags for setting PIR parameters")
-    .arg(
-      Arg::with_name("matrix_height")
-        .short("m")
-        .long("matrix_height")
-        .takes_value(true)
-        .default_value("16")
-        .help("Log2 of height of DB matrix"),
-    )
-    .arg(
-      Arg::with_name("ele_size")
-        .short("e")
-        .long("ele_size")
-        .takes_value(true)
-        .default_value("13")
-        .help("Log2 of element bit length"),
-    )
-    .arg(
-      Arg::with_name("plaintext_bits")
-        .short("p")
-        .long("plaintext_bits")
-        .takes_value(true)
-        .default_value("10")
-        .help("Number of plaintext bits encoded in each entry of DB matrix"),
-    )
-    .arg(
-      Arg::with_name("dim")
-        .short("d")
-        .long("dim")
-        .takes_value(true)
-        .default_value("2048")
-        .help("LWE dimension"),
-    )
-    .get_matches();
-
-  let ele_size =
-    parse_exp_to_usize(String::from(matches.value_of("ele_size").unwrap()));
-  let lwe_dim: usize = String::from(matches.value_of("dim").unwrap())
-    .parse()
-    .unwrap();
-  let plaintext_bits: usize =
-    String::from(matches.value_of("plaintext_bits").unwrap())
-      .parse()
-      .unwrap();
-  let m = parse_exp_to_usize(String::from(
-    matches.value_of("matrix_height").unwrap(),
-  ));
-  CLIFlags {
-    m,
-    lwe_dim,
-    plaintext_bits,
-    ele_size,
-  }
+    CLIFlags::parse()
 }
 
 pub fn parse_from_env() -> CLIFlags {
-  let ele_size = parse_exp_to_usize(env::var("PIR_ELE_SIZE_EXP").unwrap());
+  let ele_size = parse_exp_to_usize(&env::var("PIR_ELE_SIZE_EXP").unwrap()).unwrap();
   let lwe_dim: usize = env::var("PIR_LWE_DIM").unwrap().parse().unwrap();
   let plaintext_bits: usize =
     env::var("PIR_PLAINTEXT_BITS").unwrap().parse().unwrap();
-  let m = parse_exp_to_usize(env::var("PIR_MATRIX_HEIGHT_EXP").unwrap());
+  let matrix_height = parse_exp_to_usize(&env::var("PIR_MATRIX_HEIGHT_EXP").unwrap()).unwrap();
   CLIFlags {
-    m,
+    matrix_height,
     lwe_dim,
     plaintext_bits,
     ele_size,
   }
 }
 
-pub fn parse_exp_to_usize(v: String) -> usize {
-  let exp: u32 = v.parse().unwrap();
-  2_u32.pow(exp) as usize
+fn parse_exp_to_usize(v: &str) -> Result<usize, ParseIntError> {
+  let exp: u32 = v.parse()?;
+  Ok(2_u32.pow(exp) as usize)
 }
